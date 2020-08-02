5817afa38f5c5b55cce009d24bce352c1c115c8a
Andrew Lock <andrewlock.net@gmail.com>
2020-08-02 10:19:40 +0100

    Add demonstration that source generators don't work with Blazor WASM

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorldGenerated.HelloWorld.SayHello(); // calls Console.WriteLine("Hello World!") and then prints out syntax trees
+
+        }
+    }
+}
